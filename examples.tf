664763b68282537fb5d5926d34d25b5e278a81e8
Arno Schamber <arno.schamber@ideutsche-boerse.com>
2018-02-10 09:15:13 +0000

    Adding 2nd ec2 instance

--- examples.tf
+++ examples.tf
@@ -8,13 +8,26 @@ resource "aws_instance" "example" {
   ami           = "ami-1b2bb774"
   instance_type = "t2.micro"
   key_name = "clearing-vn401-sbox-euc1"
-   tags {
+  tags {
     Product = "Clearing"
     CostCenter = "6121"
     Application = "terraform"
   }
   depends_on = [ "aws_s3_bucket.ex1bucket" ]
 }
+
+resource "aws_instance" "another" {
+  # Latest Amazon AMI V2
+  ami           = "ami-1b2bb774"
+  instance_type = "t2.micro"
+  key_name = "clearing-vn401-sbox-euc1"
+  tags {
+    Product = "Clearing"
+    CostCenter = "6121"
+    Application = "terraform"
+  }
+}
+
 
 # Assign an Elastic Address to the instance
 
